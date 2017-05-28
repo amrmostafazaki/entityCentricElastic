655bcd20a5e864d451388871c6810ea9d8a9767c
Erdem Kemer <erdem.kemer@hepsiburada.com>
2017-05-28 20:34:57 +0300

    initial commit

---
+++ job_execution_log/buildEntities.sh
@@ -0,0 +1,58 @@
+echo "Indexing jobs from job_execution_log data"
+es_host="http://localhost:9200"
+updateScriptID=JobSessionUpdater
+
+
+echo "Deleting old index: jobs"
+curl -X DELETE "$es_host/jobs"
+echo ""
+echo "Creating new index: jobs"
+curl -XPUT    "$es_host/jobs" -d '
+{
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 0
+    },
+    "mappings": {
+        "job": {
+            "properties": {
+                "status": {
+                    "type": "keyword"
+                },
+                "last_start_date": {
+                    "type": "date",
+                    "format": "yyyy-MM-dd HH:mm"
+                },
+                "last_end_date": {
+                    "type": "date",
+                    "format": "yyyy-MM-dd HH:mm"
+                },
+                "job_code": {
+                    "type": "keyword"
+                },
+                "sessions": {
+                    "properties": {
+                        "session_id": {
+                            "type": "keyword"
+                        },
+                        "start_date": {
+                            "type": "date",
+                            "format": "yyyy-MM-dd HH:mm"
+                        },
+                        "end_date": {
+                            "type": "date",
+                            "format": "yyyy-MM-dd HH:mm"
+                        }
+                    }
+                },
+                "last_session_id": {
+                    "type": "keyword"
+                }
+            }
+        }
+    }
+}
+'
+echo ""
+echo "Indexing jobs from job_execution_log data"
+python ../ESEntityCentricIndexing.py events eventQuery.json job_code jobs job $updateScriptID -scriptMode incremental
---
+++ job_execution_log/jobSessionIndexer.py
@@ -0,0 +1,80 @@
+import csv
+from elasticsearch import helpers
+from elasticsearch import Elasticsearch
+import sys
+import zipfile
+import StringIO
+
+filename = 'job_execution_logs.mini.csv.zip'
+es = Elasticsearch([{'host': '192.168.56.101', 'port': 9200}])
+indexName = "job_execution_logs"
+actionsPerBulk=5000
+es.indices.delete(index=indexName, ignore=[400, 404])
+indexSettings = {
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 0
+    },
+    "mappings": {
+        "job_execution_log": {
+            "properties": {
+                "sessionId": {
+                    "type": "keyword"
+                },
+                "code": {
+                    "type": "keyword"
+                },
+                "date": {
+                    "type": "date",
+                    "format" : "yyyy-MM-dd HH:mm"
+                },
+                "message": {
+                    "type": "keyword"
+                },
+                "eventType": {
+                    "type": "integer"
+                }
+
+            }
+        }
+    }
+}
+es.indices.create(index=indexName, body=indexSettings)
+actions = []
+
+fields = []
+with zipfile.ZipFile(filename) as z:
+    for zippedFilename in z.namelist():
+        numLines = 0
+        data = StringIO.StringIO(z.read(zippedFilename))
+        reader = csv.reader(data)
+        for row in reader:
+            numLines += 1
+            if numLines == 1:
+                fields = row
+            else:
+                doc = {
+                	'sessionId': row[0],
+                 	'code': row[1],
+                 	'date':row[3],
+                    'message':row[4],
+                    'eventType':int(row[2])
+                }
+                action = {
+                        "_index": indexName,
+                        '_op_type': 'index',
+                        "_type": "job_execution_log",
+                        "_source": doc
+                }
+                actions.append(action)
+                # Flush bulk indexing action if necessary
+                if len(actions) >= actionsPerBulk:
+                    try:
+                        helpers.bulk(es, actions)
+                    except:
+                        print ("Unexpected error:", sys.exc_info()[0])
+                    del actions[0:len(actions)]
+                    print (numLines)
+
+if len(actions) > 0:
+    helpers.bulk(es, actions)
